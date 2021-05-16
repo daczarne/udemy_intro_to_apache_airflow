13c3f207f8530c7af672746782b59a0bc79d8a2a
daczarne <daniel.czarnievicz@gmail.com>
2021-05-16 22:26:56 +0200

    processing user

--- 03_create_a_data_pipeline/dags/user_processing.py
+++ 03_create_a_data_pipeline/dags/user_processing.py
@@ -1,13 +1,41 @@
 from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+from pandas import json_normalize
+import json
+
 
 #* Dictionary of default arguments. This arguments will apply to all tasks in the data pipeline.
 default_arguments = {
 	#* Data pipeline start date
 	'start_date': datetime(2020, 1, 1)
 }
+
+
+#* This function processes the user information and it will be called in the pipeline by the PythonOperator. Its argument is a TaskInstance (ti). With it, we'll be able to access the X comes.
+def _processing_user(ti):
+	#* Assign the response JSON to a variable
+	users = ti.xcom_pull(task_ids = ['extracting_user'])
+	#* Make sure the variable is not empty
+	if not len(users) or 'results' not in users[0]:
+		raise ValueError('User is empty')
+	#* Extract the user
+	user = users[0]['results'][0]
+	#* Extract the user information
+	processed_user = {
+		'first_name': user['name']['first'],
+		'last_name': user['name']['last'],
+		'country': user['location']['country'],
+		'user_name': user['login']['username'],
+		'password': user['login']['password'],
+		'email': user['email']
+	}
+	#* Convert to a pandas dataframe and store it in tmp file
+	processed_user.to_csv('/tmp/processed_user.csv', index = None, header = False)
+
 
 #* Instantiate the DAG. To do so provide the following arguments to the DAG class
 #*		DAG ID: a string with the name of the DAG. Must be unique among all DAGs.
@@ -50,8 +78,26 @@ with DAG(
 	)
 
 	#! Third task is to extracting the user from the API
-	extracting_user = HttpOperator(
-		
+	extracting_user = SimpleHttpOperator(
+		#* Set the task id
+		task_id = 'extracting_user',
+		#* Connection name
+		http_conn_id = 'user_api',
+		#* Endpoint
+		endpoint = 'api/',
+		#* Specify the API method
+		method = 'GET',
+		#* Response filter
+		response_filter = lambda response: json.loads(response.text),
+		#* Log the API response so that we can check it in the logs in case of error
+		log_response = True
 	)
 
+	#! Forth task is to process the user information. This means that we need to get only the information that we are looking for from the API response.
+	processing_user = PythonOperator(
+		#* Set the task id
+		task_id = 'processing_user',
+		#* Specify the python function that we want to call. This function needs to be defined.
+		python_callable = _processing_user
+	)
 
