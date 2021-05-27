4500ee7c37b47b73a63f64d05662f73e577b6f1e
daczarne <daniel.czarnievicz@gmail.com>
2021-05-27 23:53:07 +0200

    elastic search hook

---
+++ 06_airflow_plugins/airflow/plugins/elasticsearch_plugin/hooks/elastic_hook.py
@@ -0,0 +1,36 @@
+from airflow.hooks.base import BaseHook
+from elasticsearch import Elasticsearch
+
+class ElasticHook(BaseHook):
+
+	#* Initialize the hook
+	def __init__(self, conn_id = 'elasticsearch_default', *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		conn = self.get_connection(conn_id)
+		
+		conn_config = {}
+		hosts = []
+
+		if conn.host:
+			hosts = conn.host.split(',')
+		if conn.port:
+			conn_config['port'] = int(conn.port)
+		if conn.login:
+			conn_config['http_auth'] = (conn.login, conn.password)
+
+		self.es = Elasticsearch(hosts, **conn_config)
+		self.index = conn.schema
+
+	#* Get information about the Elasticsearch instance
+	def info(self):
+		return self.es.info()
+
+	#* Define the index
+	def set_index(self):
+		self.index = index
+
+	#* Add document to the index
+	def add_doc(self, index, doc_type, doc):
+		self.set_index(index)
+		res = self.es.index(index = index, doc_type = doc_type, doc = doc)
+		return res
